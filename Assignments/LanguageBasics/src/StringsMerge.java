45fd0c5d5ba3bcd76056ce9ae44354215c47405a
n-ananya <159942136+n-ananya@users.noreply.github.com>
2025-10-16 18:34:26 +0530

    Wipro Assignments

---
+++ Assignments/LanguageBasics/src/StringsMerge.java
@@ -0,0 +1,13 @@
+package Assignments.LanguageBasics.src;
+
+// 1
+public class StringsMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println(args[0]+" Technologies " + args[1]);
+
+	}
+
+}
---
+++ Assignments/LanguageBasics/src/WelcomeString.java
@@ -0,0 +1,13 @@
+package Assignments.LanguageBasics.src;
+
+// 2
+
+public class WelcomeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome" + args[0]);
+
+	}
+
+}
